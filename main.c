ee72f7668842d48374b852645dfcb46a808a3822
sean <seanrdev@gmail.com>
2023-09-11 16:56:49 +0000

    Adding changes to main -- more functions

--- main.c
+++ main.c
@@ -1,8 +1,34 @@
 #include <errno.h>
+#include <sys/stat.h>
+#include <sys/file.h>
 
-int main(){
+/*
+Write a function foo(int fd, char* buf, int b_size, int n, int skip)
+that reads to buf from file with file descriptor fd,
+n blocks of size b_size each. The last argument specifies
+how many bytes to skip after reading each block.
+Return -1 if the operation is unsuccessful.
+Else return total number of bytes read.
+*/
+foo(int fd, char* buf, int b_size, int n, int skip){
+	//
+}
+
+
+void makeFile(){
+	creat("asjke32.txt", S_IRWXU|S_IRGRP|S_IROTH);//7-usr:4-grp:4-oth
+	int close(fd);
+}
+
+void checkFile(){
 	if(unlink("asjke32.txt")==-1){
 		perror();
 	}
+}
+
+int main(){
+	checkFile();
+	makeFile();
+	checkFile();
 	return 0;
 }
